498d91d0bd41bf7df583c84d83da8bf74c810fc9
argue <argue@DESKTOP-VSST6CF>
2022-03-02 17:01:29 +0100

    Proyecto RodriguezBustosAlberto2122

--- src/jamon/JamonRodriguezBustosAlberto2122.java
+++ src/jamon/JamonRodriguezBustosAlberto2122.java
@@ -5,118 +5,164 @@
 package jamon;
 
 /**
- *
- * @author portatil_profesorado
+ * Clase publica JamonRodriguezBustosAlberto2122
+ * @author Rodriguez Bustos Alberto
+ * @version 1.0
  */
 public class JamonRodriguezBustosAlberto2122 {
 
     /**
-     * @return the categoria
+     * Método getter que devuelve la categoría del jamón
+     * 
+     * @return devuelve la categoria del jamón
      */
     public String getCategoria() {
         return categoria;
     }
 
     /**
-     * @param categoria the categoria to set
+     * Método setter que asigna la categoría del jamón
+     * 
+     * @param categoria asigna la categoría del jamón
      */
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
 
     /**
-     * @return the precio
+     * Método getter que devuelve el precio del jamón
+     * 
+     * @return devuelve el precio del jamón
      */
     public double getPrecio() {
         return precio;
     }
 
     /**
-     * @param precio the precio to set
+     * Métod setter que asigna el precio del jamón
+     * 
+     * @param precio asigna el precio del jamón
      */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
     /**
-     * @return the mesesCuracion
+     * Método getter que devuelve los meses de curación
+     * 
+     * @return devuelve los meses de curación
      */
     public int getMesesCuracion() {
         return mesesCuracion;
     }
 
     /**
-     * @param mesesCuracion the mesesCuracion to set
+     * Método setter que asigna los meses de curación
+     * 
+     * @param mesesCuracion asigna los meses de curación
      */
     public void setMesesCuracion(int mesesCuracion) {
         this.mesesCuracion = mesesCuracion;
     }
 
     /**
-     * @return the stock
+     * Método getter que devuelve stock
+     * @return devuelve stock
      */
     public int getStock() {
         return stock;
     }
 
     /**
-     * @param stock the stock to set
+     * metodo setter que asigna un stock
+     *
+     * @param stock indica stock
      */
     public void setStock(int stock) {
         this.stock = stock;
     }
-    
+
     private String categoria;
     private double precio;
     private int mesesCuracion;
     private int stock;
 
-    /* Constructor sin argumentos */
-    public JamonRodriguezBustosAlberto2122 ()
-    {
+    /**
+     * Constructor por defecto sin parametros
+     */
+    public JamonRodriguezBustosAlberto2122() {
     }
-    // Constructor con parámetro para iniciar todas las propiedades de la clase
-    // jamon
-    
-    public JamonRodriguezBustosAlberto2122 (String cat, double precio, int stock)
-    {
-        this.categoria =cat;
-        this.precio=precio;
-        this.stock=stock;
+
+    /**
+     * Constructor con tres parámetros para iniciar todas las propiedades de la
+     * clase jamon
+     *
+     * @param cat categoria del jamón
+     * @param precio precio del jamón
+     * @param stock cantidad de productos almacenados
+     */
+    public JamonRodriguezBustosAlberto2122(String cat, double precio, int stock) {
+        this.categoria = cat;
+        this.precio = precio;
+        this.stock = stock;
     }
-   // Método para asignar la categoría del jamón
-    public void asignarCategoria(String cat)
-    {
+
+    /**
+     * Método para asignar la categoría del jamón
+     *
+     * @param cat asigna categoria
+     */
+    public void asignarCategoria(String cat) {
         setCategoria(cat);
     }
-    // Método que me devuelve la categoría del jamón
-    public String obtenerCategoria()
-    {
+
+    /**
+     * Método que me devuelve la categoría del jamón
+     *
+     * @return devuelve categoria
+     */
+    public String obtenerCategoria() {
         return getCategoria();
     }
 
-    // Método que me devuelve el stock de jamones disponible en cada momento
-     public int obtenerStock ()
-    {
+    /**
+     * Método que me devuelve el stock de jamones disponible en cada momento
+     *
+     * @return devuelve el stock
+     */
+    public int obtenerStock() {
         return getStock();
     }
 
-    /* Método para comprar coches. Modifica el stock.
-     * Este método va a ser probado con Junit
+    /**
+     * Método para comprar jamones Modifica el stock Este método va a ser
+     * probado con Junit
+     *
+     * @param cantidad de jamones que se van a comprar
+     * @throws Exception Cuando se intenta comprar un numero negativo de jamones
      */
-    public void comprar(int cantidad) throws Exception
-    {
-        if (cantidad<0)
+    public void comprar(int cantidad) throws Exception {
+        if (cantidad < 0) {
             throw new Exception("No se puede comprar un nº negativo de jamones");
+        }
         setStock(getStock() + cantidad);
     }
 
-    public void vender (int cantidad, String paisRodriguezBustosAlberto2122) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede vender una cantidad negativa de jamones");
-        if (obtenerStock()< cantidad)
-            throw new Exception ("No  hay suficientes jamones para vender");
+    /**
+     * Método para vender jamones Modifica el stock Este método va a ser
+     * probado con Junit
+     *
+     * @param cantidad the value of cantidad
+     * @param paisRodriguezBustosAlberto2122 the value of paisRodriguezBustosAlberto2122
+     * @throws Exception
+     */
+    public void vender(int cantidad, java.lang.String paisRodriguezBustosAlberto2122) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede vender una cantidad negativa de jamones");
+        }
+        if (obtenerStock() < cantidad) {
+            throw new Exception("No  hay suficientes jamones para vender");
+        }
         setStock(getStock() - cantidad);
     }
 }
--- src/jamon/Main.java
+++ src/jamon/Main.java
@@ -5,42 +5,61 @@
 package jamon;
 
 /**
+ * Clase main del programa, que define un objeto JamonRodriguezBustosAlberto2122
+ * con unos parametros y realiza acciones de comprar y vender
  *
- * @author portatil_profesorado
+ * @author Rodriguez Bustos Alberto
+ * @version 1.0
  */
 public class Main {
-    
+
+    /**
+     * Método de la clase principal
+     * @param args argumento de linea de comando
+     */
     public static void main(String[] args) {
         JamonRodriguezBustosAlberto2122 miJamonRodriguezBustosAlberto2122;
         int stockActual;
-        
-        miJamonRodriguezBustosAlberto2122 = new JamonRodriguezBustosAlberto2122("5Jotas",580,100);
-        
-        venderJamRodriguezBustosAlberto2022(miJamonRodriguezBustosAlberto2122);  
+
+        miJamonRodriguezBustosAlberto2122 = new JamonRodriguezBustosAlberto2122("5Jotas", 580, 100);
+
+        venderJamRodriguezBustosAlberto2022(miJamonRodriguezBustosAlberto2122);
         comparJamRodriguezBustosAlberto2022(miJamonRodriguezBustosAlberto2122);
-        
+
         stockActual = miJamonRodriguezBustosAlberto2122.obtenerStock();
-        System.out.println("El stock actual es "+ stockActual );
+        System.out.println("El stock actual es " + stockActual);
     }
 
+    /**
+     * Método comparJamRodriguezBustosAlberto2122 que contiene como parametro la
+     * instancia del constructor y que comprará una cantidad de jamones
+     * determinada Tambien contiene una excepción si intentamos compra una
+     * cantidad negativa de jamones
+     *
+     * @param miJamonRodriguezBustosAlberto2122 intancia de clase
+     */
     private static void comparJamRodriguezBustosAlberto2022(JamonRodriguezBustosAlberto2122 miJamonRodriguezBustosAlberto2122) {
-        try
-        {
+        try {
             System.out.println("Compra de Jamones");
             miJamonRodriguezBustosAlberto2122.comprar(50);
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.print("Fallo al ingresar " + e);
         }
     }
 
+    /**
+     * Método venderJamRodriguezBustosAlberto2122 que contiene como parametro la
+     * instancia del constructor y que venderá una cantidad de jamones
+     * determinada Tambien contiene una excepción si nos pasamos del stock
+     * instanciado o intentamos comprar una cantidad negativa de jamones
+     *
+     * @param miJamonRodriguezBustosAlberto2122 intancia de clase
+     */
     private static void venderJamRodriguezBustosAlberto2022(JamonRodriguezBustosAlberto2122 miJamonRodriguezBustosAlberto2122) {
-        try
-        {
+        try {
             System.out.println("Venta de Jamones");
-            miJamonRodriguezBustosAlberto2122.vender(80, "España");
-        } catch (Exception e)
-        {
+            miJamonRodriguezBustosAlberto2122.vender(80, "Espana");
+        } catch (Exception e) {
             System.out.print("Fallo al vender " + e);
         }
     }
